373d2b54e37206ffb863d7a41b253cf5664a8742
THEBLUEFLASH18 <marcosgaldamez14@icloud.com>
2023-07-03 13:36:56 -0400

    Added the carousel back with a better spacing and size for the boxes

--- script.js
+++ script.js
@@ -4,4 +4,33 @@ window.addEventListener("scroll", function() {
   header.classList.toggle("sticky", window.scrollY > 0);
 });
 
+let imagesIndoor = [
+  'images/Indoor/Image4.jpeg',
+  'images/Indoor/Image5.jpeg',
+  // add more image paths
+];
+
+let imagesOutdoor = [
+  'images/Outdoor/Image15.jpeg',
+  'images/Outdoor/Image20.jpeg',
+  // add more image paths
+];
+
+let currentIndexIndoor = 0;
+let currentIndexOutdoor = 0;
+
+function changeImageIndoor() {
+  let imgElement = document.querySelector('.box:nth-child(1) .thum img');
+  imgElement.src = imagesIndoor[currentIndexIndoor];
+  currentIndexIndoor = (currentIndexIndoor + 1) % imagesIndoor.length;
+}
+
+function changeImageOutdoor() {
+  let imgElement = document.querySelector('.box:nth-child(2) .thum img');
+  imgElement.src = imagesOutdoor[currentIndexOutdoor];
+  currentIndexOutdoor = (currentIndexOutdoor + 1) % imagesOutdoor.length;
+}
+
+setInterval(changeImageIndoor, 3000); // change every 3 seconds
+setInterval(changeImageOutdoor, 3000); // change every 3 seconds
 
